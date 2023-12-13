9da75d49384b9b8d676b4cc6cc1e9bb463a5bb12
lija_lyfe <lija6556@list.ru>
2023-12-13 21:12:06 +0500

    add models Client, MailingSettings, MailingMessage, MailingLog, Mailing

--- mailing_system/mailing_app/models.py
+++ mailing_system/mailing_app/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    email = models.EmailField()
+    full_name = models.CharField(max_length=255)
+    comment = models.TextField()
+
+
+class MailingSettings(models.Model):
+    TIME_CHOICES = [
+        ('once a day', 'Once a day'),
+        ('once a week', 'Once a week'),
+        ('once a month', 'Once a month'),
+    ]
+
+    STATUS_CHOICES = [
+        ('completed', 'Completed'),
+        ('created', 'Created'),
+        ('started', 'Started'),
+    ]
+
+    sending_time = models.TimeField()
+    frequency = models.CharField(max_length=20, choices=TIME_CHOICES)
+    mailing_status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+
+
+class MailingMessage(models.Model):
+    subject = models.CharField(max_length=255)
+    body = models.TextField()
+
+
+class MailingLog(models.Model):
+    last_attempt_datetime = models.DateTimeField()
+    ATTEMPT_STATUS_CHOICES = [
+        ('success', 'Success'),
+        ('error', 'Error'),
+    ]
+    attempt_status = models.CharField(max_length=10, choices=ATTEMPT_STATUS_CHOICES)
+    mail_server_response = models.TextField()
+
+
+class Mailing(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    settings = models.ForeignKey(MailingSettings, on_delete=models.CASCADE)
+    message = models.ForeignKey(MailingMessage, on_delete=models.CASCADE)
+    logs = models.ForeignKey(MailingLog, on_delete=models.CASCADE)
+
